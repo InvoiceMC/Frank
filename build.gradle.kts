bf21cd65e7df9da7d4c7bec6024f5e2bb0317645
Azuyamat <discordazu@gmail.com>
2024-03-04 23:39:12 -0500

    build: Setting up jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import java.net.URI
+
 plugins {
+    java
     kotlin("jvm") version "1.9.22"
+    `maven-publish`
 }
 
 group = "com.invoice"
@@ -7,15 +11,45 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        url = URI("https://jitpack.io")
+        credentials {
+            username = findProperty("authToken").toString()
+        }
+    }
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.invoice"
+            artifactId = "frank"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(21)
 }
