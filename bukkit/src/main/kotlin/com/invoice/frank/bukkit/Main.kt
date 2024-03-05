56f8c32a26923fd819e4645d4288192d29b03160
Azuyamat <blaneyderek@gmail.com>
2024-03-05 11:54:05 -0500

    refactor: Just testing out common dep

--- bukkit/src/main/kotlin/com/invoice/frank/bukkit/Main.kt
+++ bukkit/src/main/kotlin/com/invoice/frank/bukkit/Main.kt
@@ -1,8 +1,11 @@
 package com.invoice.frank.bukkit
 
+import com.invoice.frank.common.Main as CommonMain
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         println("Hello test!")
+        CommonMain.main(args)
     }
 }
